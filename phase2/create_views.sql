b7db5ef9ccb9af2f1d043574bcfd9e03c1292f00
fatemeh tohidian <thdftm@gmail.com>
2023-01-08 17:41:35 +0330

    Add view defenition

--- phase2/create_views.sql
+++ phase2/create_views.sql
@@ -1,4 +1,4 @@
--- CREATE VIEW report as (
+CREATE VIEW report as (
 SELECT outer_airline.name, outer_survey.id,  outer_survey.start_time,  outer_survey.end_time, ( outer_survey.start_time < now() AND now() <  outer_survey.end_time),
     (
         SELECT COUNT(*)
@@ -20,16 +20,29 @@ SELECT outer_airline.name, outer_survey.id,  outer_survey.start_time,  outer_sur
             INNER JOIN complete on complete.survey_id = survey.id
         WHERE  outer_survey.id = survey.id
     ) total_answers,
-    (
-        SELECT COUNT(*)
+    (SELECT COUNT(*)
         FROM ticket
             INNER JOIN flight on flight.flight_number = ticket.flight_number
             INNER JOIN airline on flight.airline_name = airline.name
         WHERE outer_airline.name = airline.name
-            AND flight.date <  outer_survey.end_time
-    ) totalPossible
+            AND flight.date <  outer_survey.end_time) totalPossible,
+    (
+        (SELECT COUNT(*)
+        FROM ticket
+            INNER JOIN flight on flight.flight_number = ticket.flight_number
+            INNER JOIN airline on flight.airline_name = airline.name
+        WHERE outer_airline.name = airline.name
+            AND flight.date <  outer_survey.end_time)
+        -
+        (
+            SELECT COUNT(*)
+            FROM survey
+                INNER JOIN complete on complete.survey_id = survey.id
+            WHERE  outer_survey.id = survey.id
+        )
+    ) not_answered
 
 FROM survey outer_survey
     INNER JOIN manager on manager.username =  outer_survey.manager_username
     INNER JOIN airline outer_airline on outer_airline.name = manager.airline_name
--- )
+)
